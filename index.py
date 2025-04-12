b6a403a0ca28ac09141cb3258dfd3fdb06cfda8b
Giuliano87 <giulianojacobmatosonishi@gmail.com>
2025-04-12 18:15:01 +0900

    loserkan

--- index.py
+++ index.py
@@ -1,11 +1,10 @@
--- Active: 1744182094834@@127.0.0.1@3306@exército_brasileiro
+
 produto_1 = 10
 produto_2 = 20
-
-
-print(produto_1 + produto_2); 
-print(10-5*2); 
-print(10**(2*2));
-print(10**2*2); 
+print(produto_1 + produto_2)  # 30
+print(10 - 5 * 2)  # 6
+print(10 ** (2 * 2))  # 10000
+print(10 ** 2 * 2)  # 400
+print(10 / 2 * 4)  # 8.0    
 print(10/2*4); 
       
